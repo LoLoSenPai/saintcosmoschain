0161045497d704de30057339459f278ddec667cd
LoLoSenPai <loic.dlugosz@gmail.com>
2024-07-04 20:44:37 +0200

    add marketplace link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,17 @@ import { ConnectWallet } from "@thirdweb-dev/react";
 
 const Header = ({ theme }: { theme: any }) => {
     return (
-        <ConnectWallet className="!absolute !right-4 !top-4" theme={theme} />
+        <div className="flex flex-row md:flex-col max-sm:space-x-4 md:space-y-4 !absolute !right-4 !top-4">
+            <ConnectWallet className="mb-2 md:mb-0" theme={theme} />
+            <a
+                href="https://marketplace.witwhisker.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="!bg-blue-500 !text-white !py-2 !px-4 !rounded"
+            >
+                Go to Marketplace
+            </a>
+        </div>
     );
 };
 
